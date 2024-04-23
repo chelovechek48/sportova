0bf7c5bc82f0107868cd1550b0435167be0cb8c6
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-04-23 09:41:41 +0300

    Test yandex-tableau-widget.js

---
+++ src/assets/favicon/yandex-tableau.js
@@ -0,0 +1,12 @@
+import path from './yandex-tableau.svg';
+
+const data = {
+  version: '1.0',
+  api_version: 1,
+  layout: {
+    logo: path,
+    color: '#FFFFFF',
+  },
+};
+
+export default data;
